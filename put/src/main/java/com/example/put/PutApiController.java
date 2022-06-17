8012e23bd94d987bb7a24ddca87b4071c8c8525a
kim seongwon <seongwon1290@dsm.hs.kr>
2022-06-17 18:13:19 +0900

    🎈@PathVariable 추가

--- put/src/main/java/com/example/put/PutApiController.java
+++ put/src/main/java/com/example/put/PutApiController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class PutApiController {
 
-    @PostMapping("/put")
-    public PutRequestDto put(@RequestBody PutRequestDto requestDto){
+    @PostMapping("/put/{userId}")
+    public PutRequestDto put(@RequestBody PutRequestDto requestDto, @PathVariable(name = "userId") Long id){
         System.out.println(requestDto);
         return requestDto; //리턴 타입을 dto로 바꾸고 dto그대로 반환하면 받은값 그대로 주니까 JSON값으로 리턴
     }
