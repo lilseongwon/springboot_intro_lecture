aa0f4add44be7f7045923c00509e720c4d761a92
lilseongwon <seongwon1290@dsm.hs.kr>
2022-07-03 13:01:36 +0900

    🎈::AssertTrue로 yyyyMM검증
    Boolean 타입에는 is가 앞에, parse랑 formatter기억해놓기

--- validation2/src/main/java/com/example/validation2/User.java
+++ validation2/src/main/java/com/example/validation2/User.java
@@ -1,6 +1,8 @@
 package com.example.validation2;
 
-import javax.validation.constraints.Email;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
 
@@ -10,7 +12,20 @@ public class User {
 
     @Email(message = "이메일 양식과 맞지 않습니다.")
     private String email;
+
     private String phoneNumber;
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
+
+    @Size(max = 6, min = 6)
+    private String reqYearMonth; //yyyyMM
+
 
     public String getName() {
         return name;
@@ -45,4 +60,27 @@ public class User {
         this.phoneNumber = phoneNumber;
     }
 
+    @AssertTrue(message = "yyyyMM의 형식에 맞지 않습니다.")
+    public boolean isReqYearMonthValidation(){ //boolean 타입은 앞에 is를 붙여줘야함
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+            //parse = 포매터를 사용하여 텍스트 문자열에서 LocalDate인스턴스를 가져옴. 텍스트는 날짜를 반환.
+            //년원일 패턴으로 텍스트에서 가져올 떄 try/catch 돌림
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
+                '}';
+    }
 }
+
